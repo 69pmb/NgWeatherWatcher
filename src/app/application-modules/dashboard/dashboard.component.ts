713ea2d4ac632bb4e07ab2263c5987b88e506887
pierre-marie broca <pierre-marie.broca@worldline.com>
2021-03-03 14:11:45 +0100

    feat: display forecast on dashboard

--- src/app/application-modules/dashboard/dashboard.component.ts
+++ src/app/application-modules/dashboard/dashboard.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
+import { Forecast } from '../../model/forecast';
+import { WeatherService } from '../../shared/shared.module';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
+  forecast: Forecast;
+  subs: Subscription[] = [];
 
-  constructor() { }
+  constructor(
+    private weatherService: WeatherService,
+    private translate: TranslateService
+  ) { }
 
   ngOnInit(): void {
+    this.subs.push(this.weatherService.findForecastByLocation('', '5', this.translate.currentLang)
+      .subscribe(forecast => this.forecast = forecast));
+  }
+
+  ngOnDestroy(): void {
+    this.subs.forEach(sub => sub.unsubscribe);
   }
 
 }
